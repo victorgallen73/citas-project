3d64b64c3a542e5c03802f31e0c8fd80649fc82f
Victor Gallen <victorgallenrenau@gmail.com>
2023-01-10 17:42:49 +0100

    Added style to Patient List

---
+++ src/components/Patient.jsx
@@ -0,0 +1,27 @@
+
+const Patient = () => {
+  return (
+    <div className="mt-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase"> Pet name: {''}
+          <span className="font-normal normal-case">Nubba</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase"> Owner name: {''}
+          <span className="font-normal normal-case">Victor</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase"> Email: {''}
+          <span className="font-normal normal-case">victor@email.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase"> Entry date: {''}
+          <span className="font-normal normal-case">10/12/2019</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase"> Symptom: {''}
+          <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Vero excepturi dolores corrupti fugiat facere dolorum! Ad, aliquid excepturi id recusandae corrupti quos,
+            sint, culpa nobis delectus ipsum voluptatem pariatur repellat.
+          </span>
+        </p>
+      </div>
+  )
+}
+
+export default Patient
--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,8 +1,21 @@
+import Patient from "./Patient"
+
 const PatientsList = () => {
   return (
-    <>
-      <h2>Patients List</h2>
-    </>
+    <div className="md:w-1/2 lg:w-3/5 ml-2 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">Patients List</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Manage your {''}
+        <span className="text-indigo-600 font-bold">
+          Patients and Appointments
+        </span>
+      </p>
+      <Patient />
+      <Patient />
+      <Patient />
+      <Patient />
+      <Patient />
+    </div>
   )
 }
 
