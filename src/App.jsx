624be81fd8c3c2032399e0f42ef8c567708e4326
Victor Gallen <victorgallenrenau@gmail.com>
2023-01-11 18:17:48 +0100

    Add patients to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import PatientsList from "./components/PatientsList";
 
 function App() {
 
-  const [patients, setPatients] = useState([]);
+  const [patients, setPatients] = useState(JSON.parse(localStorage.getItem('patients')) ?? []);
   const [patient, setPatient] = useState({});
+
+  useEffect( () => {
+    localStorage.setItem('patients', JSON.stringify(patients));
+  }, [patients]);
 
   const deletePatient = (id) => {
     const updatedPatients = patients.filter( patientState => patientState?.id !== id);
